38ebe1779da63413886ccba6b6dccd0f67e50780
SV <sv@SVs-MacBook-Air.local>
2017-01-02 15:20:35 +0100

    new: create local dump of the portal datasets

--- get_portal_datasets.py
+++ get_portal_datasets.py
@@ -12,19 +12,27 @@ Collect metadata of the portal datasets
 '''
 
 import requests
-
+import pickle
+# from generate_wordcloud import generate_wordcloud
 
 PORTAL_WATCH_API = 'http://data.wu.ac.at/portalwatch/api/v1/portal/'
 
 
 def get_descriptions_of_all_portal_datasets(portal_id, snapshot='1643'):
     # Get the list of all portal datasets, e.g. http://data.wu.ac.at/portalwatch/api/v1/portal/www_opendataportal_at/1643/datasets
-    datasets_api = PORTAL_WATCH_API+'/'.join([portal_id, snapshot, 'datasets'])
-    response = requests.get(datasets_api)
-    datasets = response.json()
+    datasets_dump = 'datasets/'+portal_id+'_'+snapshot+'.json'
+    try:
+        with open(datasets_dump, 'r') as infile:
+            datasets = pickle.load(infile)
+    except:
+        datasets_api = PORTAL_WATCH_API+'/'.join([portal_id, snapshot, 'datasets'])
+        response = requests.get(datasets_api)
+        datasets = response.json()
+        with open(datasets_dump, 'w') as outfile:
+            pickle.dump(datasets, outfile)
     # Using "id", get the metadata for a dataset, e.g. http://data.wu.ac.at/portalwatch/api/v1/portal/www_opendataportal_at/1643/dataset/c4feea62-92d8-4855-a2b5-98338a0ffe41
     descriptions = []
-    for dataset in datasets[:10]:
+    for dataset in datasets[:2]:
         dataset_id = dataset['id']
         meta_api = PORTAL_WATCH_API+'/'.join([portal_id, snapshot, 'dataset', dataset_id])
         response = requests.get(meta_api)
@@ -40,10 +48,11 @@ def get_descriptions_of_all_portal_datasets(portal_id, snapshot='1643'):
 
 def test_get_descriptions_of_all_portal_datasets():
     # data.gv.at
-    # portal_id = 'data_gv_at'
-    portal_id = 'www_opendataportal_at'
+    portal_id = 'data_gv_at'
+    # portal_id = 'www_opendataportal_at'
     descriptions = get_descriptions_of_all_portal_datasets(portal_id)
     print descriptions
+    # generate_wordcloud(' '.join(descriptions))
 
 
 if __name__ == '__main__':
